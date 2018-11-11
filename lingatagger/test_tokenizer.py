ea7821930963a0a446bcbeb9ee363f0538de1672
Samriddhi Sinha <samriddhidjokestersinha@gmail.com>
2018-11-12 02:20:42 +0530

    Add Tests for Tokenizers

---
+++ lingatagger/test_tokenizer.py
@@ -0,0 +1,15 @@
+from . import tokenizer
+def test_tokenize():
+    input_str = 'Hey there! Wassup? \n Nice Meeting You.\t Have a nice day.'
+    out = tokenizer.tokenize(input_str)
+    assert out == ['Hey', 'there', '!', 'Wassup', '?', 'Nice', 'Meeting', 'You', '.', 'Have', 'a', 'nice', 'day', '.']
+
+def test_wordtokenize():
+    input_str = 'Hey there! Wassup? \n Nice Meeting You.\t Have a nice day.'
+    out = tokenizer.wordtokenize(input_str)
+    assert out == ['Hey', 'there', 'Wassup', 'Nice', 'Meeting', 'You', 'Have', 'a', 'nice', 'day']
+
+def test_senttokenize():
+    input_str = 'Hey there! Wassup? \n Nice Meeting You.\t Have a nice day.'
+    out = tokenizer.sentencetokenize(input_str)
+    assert out == ['Hey there! ', 'Wassup? \n ', 'Nice Meeting You.\t ', 'Have a nice day.']
