003694631fc71032d5d95c1b9aff2a172475f40d
Samriddhi Sinha <samriddhidjokestersinha@gmail.com>
2018-11-12 22:31:01 +0530

    Update test_tokenizer.py

--- lingatagger/test_tokenizer.py
+++ lingatagger/test_tokenizer.py
@@ -1,4 +1,4 @@
-from . import tokenizer
+import lingatagger.tokenizer as tokenizer
 def test_tokenize():
     input_str = 'Hey there! Wassup? \n Nice Meeting You.\t Have a nice day.'
     out = tokenizer.tokenize(input_str)
